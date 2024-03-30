f3d99dc1c35513d58a618610a298360f28418c2d
Federico Magnani <drun@ip-192-168-50-134.ap-northeast-1.compute.internal>
2024-03-30 17:13:12 +0100

    search for gfortran

--- build.rs
+++ build.rs
@@ -60,15 +60,19 @@ fn main() {
         println!("cargo:rustc-link-search={}", fc_lib_pwd.to_str().unwrap());
     }
 
-    if os == "Macos" {
-        // println!("cargo:rustc-link-search=/opt/homebrew/bin");
-        println!(
-            "cargo:rustc-link-lib={}=/opt/homebrew/bin/gfortran",
-            fc_lib_type
-        );
-    } else {
-        println!("cargo:rustc-link-lib={}=gfortran", fc_lib_type);
-    }
+    // if os == "Macos" {
+    //     // println!("cargo:rustc-link-search=/opt/homebrew/bin");
+    //     println!(
+    //         "cargo:rustc-link-lib={}=/opt/homebrew/bin/gfortran",
+    //         fc_lib_type
+    //     );
+    // } else {
+    //     println!("cargo:rustc-link-lib={}=gfortran", fc_lib_type);
+    // }
+    println!(
+        "cargo:rustc-link-lib={}=/opt/homebrew/bin/gfortran",
+        fc_lib_type
+    );
 
     if target == "x86_64-apple-darwin" {
         println!("cargo:rustc-link-lib={}=quadmath", fc_lib_type);
